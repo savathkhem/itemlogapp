6b452fe9efeeaf8ba0034dfd5e74abb1e38aff39
Savath <savath.khem@live.com>
2019-09-05 08:52:23 -0400

    navbar

--- src/index.js
+++ src/index.js
@@ -2,6 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import * as firebase from 'firebase';
+
+var firebaseConfig = {
+    apiKey: "AIzaSyAZYLec00OoaJqs3ZnUxovgVHy0QFNoL74",
+    authDomain: "itemlogapp-9a157.firebaseapp.com",
+    databaseURL: "https://itemlogapp-9a157.firebaseio.com",
+    projectId: "itemlogapp-9a157",
+    storageBucket: "",
+    messagingSenderId: "628887270649",
+    appId: "1:628887270649:web:be2faa642968a5db715e28"
+  };
+  // Initialize Firebase
+
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
